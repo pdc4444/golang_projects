8a238ba95243ca252536edc60b62ca0fbff3e9c6
Peter Cooper <peter.cooper@elephantbox.net>
2020-11-05 19:24:58 -0500

    Updated the comments at the top of the file, fixed some spelling errors.

--- oracle_challenge/oracle_challenge.go
+++ oracle_challenge/oracle_challenge.go
@@ -1,5 +1,29 @@
 package main
 
+/*
+
+Overview:
+	Create a simple webpage that when accessed will return the client's IP address and location data.
+
+Special Requirements:
+	The tool should return identical IP data from what is observed at http://checkip.dyndns.com/
+	The tool should use the "X-FORWARDED-FOR" value for it's IP address if it is present in the client request
+
+Devloper: Peter Cooper
+
+Sources Used:
+https://www.youtube.com/watch?v=YS4e4q9oBaU
+https://golang.org/pkg/net/http/
+https://tutorialedge.net/golang/creating-simple-web-server-with-golang/
+https://stackoverflow.com/questions/41240761/check-if-ip-address-is-in-private-network-space
+http://checkip.dyndns.com/
+https://appdividend.com/2019/12/02/golang-http-example-get-post-http-requests-in-golang/
+https://www.alexedwards.net/blog/how-to-properly-parse-a-json-request-body
+https://ipinfo.io/developers#json-response
+https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Forwarded-For
+
+*/
+
 import (
 	"encoding/json"
 	"errors"
@@ -20,10 +44,12 @@ type geolocation struct {
 	City     string
 }
 
-// The main func creates an http.server at :8080/ip
-// When a request is served data is pulled from the client to determine it's IP address and geolocation
-// The IP address and geo location are then returned back to the client via fmt.FprintF
-// Any errors encountered while processing the IP address or geo location, bubble up to the surface and are displayed for the client
+/*
+	The main func creates an http.server at http://127.0.0.1:8080/ip
+	When a request is served, data is pulled from the client to determine it's IP address and geolocation
+	The IP address and geo location are then returned back to the client via fmt.FprintF (easily visible through a web browser)
+	Any errors encountered while processing the IP address / geo location, bubble up to the surface and are displayed for the client
+*/
 func main() {
 	http.HandleFunc("/ip", func(w http.ResponseWriter, r *http.Request) {
 		ip, err := determineIP(r)
@@ -42,9 +68,11 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-// The determineGeoLocation function takes an IP address and sends a request to the ipinfo API
-// When a successful response is received from the API the JSON array is decoded through use of buildGeolocation()
-// Location data is then concatenated and returned
+/*
+	The determineGeoLocation function takes an IP address and sends a request to the ipinfo API
+	When a successful response is received from the API the JSON array is decoded through use of buildGeolocation()
+	Location data is then concatenated and returned
+*/
 func determineGeoLocation(ip string) (string, error) {
 
 	url := "http://ipinfo.io/" + ip
@@ -63,10 +91,12 @@ func determineGeoLocation(ip string) (string, error) {
 	return locationData, nil
 }
 
-// The determineIP function takes an http.Request struct and retrieves the value for X-FORWARDED-FOR header key as well as http.Request.RemoteAddr
-// If the X-FORWARDED-FOR header key is set and the content is determined to be a valid ip address, we return this address in string form
-// else we validate the IP address contained within http.Request.RemoteAddr, if we find that it is within a private subnet then the external IP adress is returned through use of acquireExternalIP()
-// else we just return the ip found in http.Request.RemoteAddr
+/*
+	The determineIP function takes an http.Request struct and retrieves the value for X-FORWARDED-FOR header key as well as http.Request.RemoteAddr
+	If the X-FORWARDED-FOR header key is set and the content is determined to be a valid ip address, we return this address in string form
+	else we validate the IP address contained within http.Request.RemoteAddr, if we find that it is within a private subnet then the external IP address is returned through use of acquireExternalIP()
+	else we just return the ip found in http.Request.RemoteAddr
+*/
 func determineIP(request *http.Request) (string, error) {
 
 	// Obtain a slice of IP addresses if information is found within the X-FORWARDED-FOR header
@@ -107,12 +137,14 @@ func determineIP(request *http.Request) (string, error) {
 	return "", errors.New("a valid IP address was not found")
 }
 
-// The determinePrivacy function builds a slice of *net.IPNet subnets via net.ParseCIDR
-// We then loop through each IPNet struct and use the IPNet.Contains() function to see if the passed net.IP is within a private subnet.
-// We're really just looking to receive a boolean from this function to know if acquireExternalIP() will need to be called
+/*
+	The determinePrivacy function builds a slice of *net.IPNet subnets via net.ParseCIDR
+	We then loop through each IPNet struct and use the IPNet.Contains() function to see if the passed net.IP is within a private subnet.
+	We're really just looking to receive a boolean from this function to know if acquireExternalIP() will need to be called
+*/
 func determinePrivacy(ip net.IP) (bool, error) {
 
-	adressesCIDR := []string{
+	addressesCIDR := []string{
 		"127.0.0.0/8",    // IPv4 loopback
 		"10.0.0.0/8",     // RFC1918
 		"172.16.0.0/12",  // RFC1918
@@ -123,7 +155,7 @@ func determinePrivacy(ip net.IP) (bool, error) {
 	var privateRanges []*net.IPNet
 
 	// Compile the list of parsed subnets based upon the CIDR slice above
-	for _, stringCIDR := range adressesCIDR {
+	for _, stringCIDR := range addressesCIDR {
 		_, networkRange, err := net.ParseCIDR(stringCIDR)
 		if err != nil {
 			return false, err
@@ -154,8 +186,10 @@ func acquireExternalIP() (string, error) {
 	return jsonResponse.IP, nil
 }
 
-// The buildGeoLocation function takes and http.Response and builds a geolocation struct.
-// It's expected that the http.Response is the product of an API in JSON format
+/*
+	The buildGeoLocation function takes and http.Response and builds a geolocation struct.
+	It's expected that the http.Response is the product of an API in JSON format
+*/
 func buildGeolocation(response *http.Response) (geolocation, error) {
 	var jsonResponse geolocation
 	err := json.NewDecoder(response.Body).Decode(&jsonResponse)
